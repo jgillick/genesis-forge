8a3dcc1a355d5200d3eb2348a11f2013328474b0
Jeremy Gillick <j_gillick@yahoo.com>
2026-08-29 12:16:14 -0700

    Update the wheeled robot to be a 4-wheel system

--- examples/wheeled_robot/train.py
+++ examples/wheeled_robot/train.py
@@ -18,7 +18,7 @@ EXPERIMENT_NAME = "wheeled-robot-command"
 
 parser = argparse.ArgumentParser(add_help=True)
 parser.add_argument("-n", "--num_envs", type=int, default=4096)
-parser.add_argument("--max_iterations", type=int, default=350)
+parser.add_argument("--max_iterations", type=int, default=400)
 parser.add_argument("-d", "--device", type=str, default="gpu")
 parser.add_argument("-e", "--exp_name", type=str, default=EXPERIMENT_NAME)
 args = parser.parse_args()
